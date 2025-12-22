3cbbb592682c729030d031486b196292825ec185
iapetus-star <amanullahayam@gmail.com>
2025-12-22 15:53:38 -0700

    fixed a syntax error that broke js

--- webDevelopment/index.js
+++ webDevelopment/index.js
@@ -6,7 +6,7 @@ const SUPABASE_URL = "https://aynvmshmrcxcccglxcdk.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5bnZtc2htcmN4Y2NjZ2x4Y2RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyNzEzMjgsImV4cCI6MjA4MTg0NzMyOH0.JAShR_lIGbv7MVUaiMf5qm1ufEFTXbwL6Rs4R1CYL-M";
 
 // CREATE SUPABASE CLIENT
-const supabase = supabase.createClient(
+const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
@@ -15,7 +15,7 @@ const supabase = supabase.createClient(
 async function loadRecentItems() {
   const grid = document.getElementById("items-grid");
 
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from("items")
     .select("*")
     .eq("claimed", false) // // REMOVE IF YOU DON'T HAVE THIS COLUMN
